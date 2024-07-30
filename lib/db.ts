619c825f51ef84bfbe7088beea94a6953ae9b7ae
Sayan Majumder <sayanmajumder0002@gmail.com>
2024-07-31 04:34:10 +0530

    webhook fixes 5

--- lib/db.ts
+++ lib/db.ts
@@ -1,50 +1,19 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL!;
 
-interface MongooseConn {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
-
-// Check if there is already a global cached connection
-let cached: MongooseConn = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
 export const connect = async () => {
-  // Log the cached connection
-  console.log("cached.conn", cached.conn);
-
-  // If there is already a cached connection, return it
-  if (cached.conn) return cached.conn;
-  
-  // Log the MongoDB URL being used
-  console.log("MONGODB_URL", MONGODB_URL);
-
-  // If there is no cached promise, create a new one
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URL, {
+  try {
+    console.log("MONGODB_URL", MONGODB_URL);
+    const conn = await mongoose.connect(MONGODB_URL, {
       dbName: "User",
       bufferCommands: false,
       connectTimeoutMS: 30000,
     });
+    console.log("Connected to MongoDB:", conn.connection.host);
+    return conn;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("Could not connect to MongoDB");
   }
-
-  // Log the cached promise
-  console.log("cached.promise", cached.promise);
-
-  // Await the promise and cache the connection
-  cached.conn = await cached.promise;
-
-  // Log the cached connection after it has been established
-  console.log("cached.conn", cached.conn);
-
-  // Return the cached connection
-  return cached.conn;
 };
